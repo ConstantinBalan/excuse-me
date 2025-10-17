444541ed7bd3d8badbcfb1258a17f46d9683e1da
Cristian Balan <cristian.daniel.balan@gmail.com>
2025-10-17 16:54:16 -0400

    card state scripts added
    Added card_base_state, card_clicked_state, card_dragging_state, card_released_state.
    
    Functionality doesn't work yet need to look into what is causing this.

--- game_objects/card/card.gd
+++ game_objects/card/card.gd
@@ -3,6 +3,7 @@ extends Node
 
 @onready var card_image: TextureRect = $TextureRect
 @onready var card_name: Label = $State
+@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 
 @export var card_data: CardStats
 
@@ -11,3 +12,16 @@ signal reparent_requested(which_card_ui: Card)
 func _ready():
 	card_image.texture = card_data.card_texture
 	card_name.text = card_data.card_name
+	card_state_machine.init(self)
+
+func _input(event: InputEvent) -> void:
+	card_state_machine.on_input(event)
+
+func _on_gui_input(event: InputEvent) -> void:
+	card_state_machine.on_gui_input(event)
+
+func _on_mouse_entered() -> void:
+	card_state_machine.on_mouse_entered()
+
+func _on_mouse_exited() -> void:
+	card_state_machine.on_mouse_exited()
